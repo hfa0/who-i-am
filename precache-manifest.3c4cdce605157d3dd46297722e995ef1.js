501aeb509cabd9f650d41f63f89d9a5356bbf328
hfa0 <fakhir.husein@gmail.com>
2020-05-24 15:16:48 +0200

    Updates

---
+++ precache-manifest.3c4cdce605157d3dd46297722e995ef1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd3c3f0a0b80e19805afcbb4c23efcf",
+    "url": "/who-i-am/index.html"
+  },
+  {
+    "revision": "9c0664d2bc4173235df4",
+    "url": "/who-i-am/static/css/main.f44d16ee.chunk.css"
+  },
+  {
+    "revision": "859baf7e9e488b2a18c6",
+    "url": "/who-i-am/static/js/2.43bfe37f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/who-i-am/static/js/2.43bfe37f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0664d2bc4173235df4",
+    "url": "/who-i-am/static/js/main.3b14e8c7.chunk.js"
+  },
+  {
+    "revision": "bc5f5db4c499661afcc5",
+    "url": "/who-i-am/static/js/runtime-main.0bd073cd.js"
+  },
+  {
+    "revision": "30718714f50f08fd58ac1b32888211e2",
+    "url": "/who-i-am/static/media/bitmovin.30718714.png"
+  },
+  {
+    "revision": "9db9f841d30db88765e11c24830e2bb6",
+    "url": "/who-i-am/static/media/etadex.9db9f841.png"
+  },
+  {
+    "revision": "b52c55ed2003c3090abf762a4f019a38",
+    "url": "/who-i-am/static/media/hokify.b52c55ed.png"
+  }
+]);
