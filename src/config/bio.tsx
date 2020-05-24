5df901afbd5e6e51a1c9b37629642e17c8f02a2a
hfa0 <fakhir.husein@gmail.com>
2020-05-24 15:23:48 +0200

    remove my config

--- src/config/bio.tsx
+++ src/config/bio.tsx
@@ -1,32 +1,4 @@
 import React from "react";
 import { myAge } from "../utils";
 
-export default () => (
-  <>
-    <p>Hi visitors, </p>
-    I'm happy you get lost in this jungle of connected networks and finally landed on my page. My name is Husein. I'm{" "}
-    {myAge()} years old and I'm a Software Developer.
-    <p>
-      I love to write code, learn new things and read good books. My favorite one is War and Pease. I love the way
-      Tolstoy describes situations and peoples personalities. It's like I have pictures of every single character he
-      mentioned in this book - Just joking. There are so many I couldn't even count them.
-    </p>
-    <p>
-      My greatest idol is Ignatiy Stelletsky. I do really respect people who fight for what makes them happy, even that
-      happiness was finding the truth and he didn't succeed (sorry bro, I can really feel you. These books in that
-      mysteriously lost library from one of the most terrible guys ever lived must be f**ing awesome).
-    </p>
-    <p>
-      My favorite movies are <strong>Interstellar</strong> and <strong>No Country for Old Men</strong>. I'm very
-      fascinated about the universe, maths and physics. And of course, I love computers - how one couldn't? Still, I'm
-      kind of an old-fashioned person. I love greek philosophy and the way antic civilizations viewed the world. From
-      the Mayan to the Babylonian, Egyptian, Hindu and Roman - What we learned in the past will help us improve in the
-      future.
-    </p>
-    <p>
-      When I'm not writing code, I like to explore the world. I like to go to places few people have been. I love
-      beaches, lakes and every place where there is water. I love plants and it makes me happy watching them growing and
-      blooming.
-    </p>
-  </>
-);
+export default () => <p>Hello world, I'm {myAge()} years old</p>;
